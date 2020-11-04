b1432d8d133f1fb8aed35429889fc13ba08513d6
Luka Golic <luka.licgo@gmail.com>
2020-11-04 19:46:39 +0100

    Dodano bacanje i skupljanje smeca

---
+++ Clean Earth/Assets/Scripts/Inventory.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //public static int inventoryNum = 10;
+    public GameObject[] inventory = new GameObject[10];
+   
+    
+
+    public void AddItem(GameObject item)
+    {
+        bool isFull = false;
+        for(int i = 0; i<inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                inventory[i] = item;
+                Debug.Log(item.name + " is added");
+                isFull = true;
+                item.SendMessage("DoInteraction");
+                break;
+            }
+        }
+        if (!isFull)
+        {
+            Debug.Log("Inventory is full");
+        }
+        
+        
+    }
+    //za sada baca prvi item po redu iz inventory-a
+    public void ThrowItem()
+    {
+        if (isInventoryEmpty())
+        {
+            Debug.Log("Inventory is empty");
+        }
+        else
+        {
+            GameObject item = inventory[0];
+            SpriteRenderer r = gameObject.GetComponent<SpriteRenderer>();
+            Debug.Log(r.sprite);
+            //Debug.Log(gameObject.transform.position.x + " " + gameObject.transform.position.y + " " + item.transform.position.z);
+            Vector3 newPosition = new Vector3((float)(gameObject.transform.position.x), (float)(gameObject.transform.position.y), item.transform.position.z);
+            item.transform.position = newPosition;
+            //item.SendMessage("Throw");
+            item.SetActive(true);
+
+            GameObject[] inventoryTmp = new GameObject[10];
+            for(int i = 1; i<inventory.Length; i++)
+            {
+                inventoryTmp[i - 1] = inventory[i];
+            }
+
+            inventory = inventoryTmp;
+
+        }
+    }
+
+    public bool isInventoryEmpty()
+    {
+        
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] != null)
+            {
+                Debug.Log("Inventory ima nesto");
+                return false;
+            }
+        }
+        Debug.Log("Inventory je prazan");
+        return true;
+    }
+}
---
+++ Clean Earth/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+    public InteractionObject currentInterObjScript = null;
+    public Inventory inventoryScript;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("InteractUp") &&  currentInterObj)
+        {
+            if (currentInterObjScript.inventory)
+            {
+                inventoryScript.AddItem(currentInterObj);
+            }
+            
+
+        }
+        if (Input.GetButtonDown("InteractDown"))
+        {
+            Debug.Log("Krece bacati");
+            inventoryScript.ThrowItem();
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("interactObject"))
+        {
+            Debug.Log(collision.name);
+            currentInterObj = collision.gameObject;
+            currentInterObjScript = currentInterObj.GetComponent<InteractionObject>();
+
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("interactObject"))
+        {
+            if(collision.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+                currentInterObjScript = null;
+            }
+        }
+    }
+}
